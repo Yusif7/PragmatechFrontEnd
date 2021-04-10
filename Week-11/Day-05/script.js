f5548992e8e85053fef6d6f2e9e0281b481a070d
Yusif Osmanov <o.yusif@hotmail.com>
2021-04-10 23:39:22 +0400

    Keep It Straight

--- Week-11/Day-05/script.js
+++ Week-11/Day-05/script.js
@@ -1,3 +1,27 @@
-document.querySelector('.container').addEventListener('mouseenter', () => {
-
+let container = document.querySelector('.container');
+let resultText = document.querySelector('h1')
+let winner = false;
+container.addEventListener('mouseover', (e) => {
+    if (e.target.getAttribute('data-filter') == 'start') {
+        container.addEventListener('mouseover', (ev) => {
+            if (ev.target.getAttribute('data-filter') == 'middle') {
+                container.addEventListener('mouseout', (event) => {
+                    if (event.target.getAttribute('data-filter') == 'end') {
+                        winner = true;
+                        checkWinner(winner, resultText)
+                    }
+                })
+            }
+        })
+    }
 })
+
+
+const checkWinner = (elem, text) => {
+    if (elem == false) {
+        text.innerText = 'You lose..Try again';
+        return 0
+    } else {
+        text.innerText = 'You win!!!'
+    }
+}
